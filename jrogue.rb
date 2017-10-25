b62e9c042d07d2555ec77d06b639adef4d75049c
leopard-gecko <kaz_bashi@yahoo.co.jp>
2017-10-25 22:31:22 +0900

    Update jrogue.rb

--- jrogue.rb
+++ jrogue.rb
@@ -1,10 +1,11 @@
 class Jrogue < Formula
   homepage "http://leopardgecko.zouri.jp/rogue.html"
-  url "http://file.leopardgecko.blog.shinobi.jp/jrogue026.tar.gz"
-  version "5.4.5J.026"
-  sha256 "0fc5cd240ff99317c6bfd1ec2325630415e199ea3ac97665d08935e17e19b114"
+  url "http://file.leopardgecko.blog.shinobi.jp/jrogue027.tar.gz"
+  version "5.4.5J.027"
+  sha256 "bb321a574283df3689a49163a24227c6663e56729765b62948c3bf151e44833d"
 
-  option "without-bg2black", "背景色を黒に変更しない"
+  option "without-bg2black", "背景色を変更しない"
+  option "without-nocursor", "カーソルを表示する"
 
   def install
     args = %W[
@@ -12,9 +13,10 @@ class Jrogue < Formula
       --mandir=/usr/local/share/man/ja/man6
     ]
     args << "--enable-bg2black=no" if build.without? "bg2black"
+    args << "--enable-nocursor=no" if build.without? "nocursor"
     
     system "./configure", *args
-    system "make", "install" # if this fails, try separate make/make install steps
+    system "make", "install"
   end
 
   test do
