7aeacb56b46235e2f2124835b848999a3dde5c11
leopard-gecko <23162102+leopard-gecko@users.noreply.github.com>
2026-06-16 14:24:29 +0900

    Update SHA256 checksum for version 5.4.5

--- jrogue.rb
+++ jrogue.rb
@@ -3,7 +3,7 @@ class Jrogue < Formula
   homepage "https://leopard-gecko.github.io/jrogue/"
   url "https://github.com/leopard-gecko/homebrew-game/releases/download/v5.4.5.jec.050/rogue_545_jec_050.tar.gz"
   version "5.4.5jec.050"
-  sha256 "d514a714c8860fc30694d44b2325dac348b112b3a7f71efddc80f841971fb0ed"
+  sha256 "d6eb5e3fc4386f64268dce65e2f85e2ee2b4cb27db92a70f7f8b7bc9bbf89894"
 
   option "with-modern-settings", "カラー有効、passgo有効、識別の巻き物1種類、カーソル非表示を初期設定にしてビルドする"
   option "with-japanese", "日本語を初期言語にしてビルドする"
