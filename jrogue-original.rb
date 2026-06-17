cfb4cdbd071421523973db3b305c97011aa3f205
leopardgecko <23162102+leopard-gecko@users.noreply.github.com>
2026-06-17 16:56:27 +0900

    Formulaを分割

---
+++ jrogue-original.rb
@@ -0,0 +1,21 @@
+class JrogueOriginal < Formula
+  desc "Rogue 5.4.5 with original-like defaults and macOS fixes"
+  homepage "https://leopard-gecko.github.io/jrogue/"
+  url "https://github.com/leopard-gecko/homebrew-game/releases/download/v5.4.5.jec.050/rogue_545_jec_050.tar.gz"
+  version "5.4.5jec.050"
+  sha256 "b72f835b3b434ef6350cd6cc8a1e48d7f92671f84fdc7c78387287fea988a14b"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --program=jrogue-original
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/jrogue-original", "-s"
+  end
+end
---
+++ jrogue-wizard.rb
@@ -0,0 +1,23 @@
+class JrogueWizard < Formula
+  desc "Rogue 5.4.5 with wizard mode, color, and macOS fixes"
+  homepage "https://leopard-gecko.github.io/jrogue/"
+  url "https://github.com/leopard-gecko/homebrew-game/releases/download/v5.4.5.jec.050/rogue_545_jec_050.tar.gz"
+  version "5.4.5jec.050"
+  sha256 "b72f835b3b434ef6350cd6cc8a1e48d7f92671f84fdc7c78387287fea988a14b"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --program=jrogue-wizard
+      --modern
+      --wizard
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/jrogue-wizard", "-s"
+  end
+end
