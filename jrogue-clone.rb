bc74e51f03714d88a2db0c1481b4e903032388cf
leopardgecko <23162102+leopard-gecko@users.noreply.github.com>
2026-06-18 11:28:41 +0900

    Create jrogue-clone.rb

---
+++ jrogue-clone.rb
@@ -0,0 +1,25 @@
+class JrogueClone3 < Formula
+  desc "Rogue Clone III with Japanese, color, and macOS fixes"
+  homepage "https://leopard-gecko.github.io/jrogue/"
+  url "https://github.com/leopard-gecko/homebrew-game/releases/download/vclone-iii.jec.001/rogue_clone_iii_jec_001.tar.gz"
+  version "clone-iii.jec.001"
+  sha256 "727ed6506364c320fbe099c5e9dff3922c3e4683cea8cef04f78f9dab0ba9777"
+
+  def install
+    args = %W[
+      --prefix=#{prefix}
+      --program=jrogue-clone
+      --docdir=#{share}/doc/jrogue-clone
+      --save-file=rogue-clone.save
+      --score-file=rogue-clone.scores
+      --lock-file=rogue-clone.lock
+    ]
+
+    system "./configure", *args
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/jrogue-clone", "-s"
+  end
+end
