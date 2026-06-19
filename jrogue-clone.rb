bb4fa0af8b8c24175dbf65f847e45988add22fd9
leopard-gecko <23162102+leopard-gecko@users.noreply.github.com>
2026-06-19 21:34:45 +0900

    Update SHA256 checksum for rogue_clone_iii

--- jrogue-clone.rb
+++ jrogue-clone.rb
@@ -3,7 +3,7 @@ class JrogueClone < Formula
   homepage "https://leopard-gecko.github.io/jrogue/"
   url "https://github.com/leopard-gecko/homebrew-game/releases/download/vclone-iii.jec.001/rogue_clone_iii_jec_001.tar.gz"
   version "clone-iii.jec.001"
-  sha256 "ed57055c35e564285da7755b22ece04a1333d1de92795884292893399bfa9567"
+  sha256 "fd0364bec65da89639dd4e741f6ca8fc9f7dbd9d02b4942d23a7bf4f74cfe5fa"
 
   def install
     args = %W[
@@ -13,6 +13,7 @@ class JrogueClone < Formula
       --save-file=rogue-clone.save
       --score-file=rogue-clone.scores
       --lock-file=rogue-clone.lock
+      --modern
     ]
 
     system "./configure", *args
