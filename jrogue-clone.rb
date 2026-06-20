64e34798f5f5092e1b25d89b4ccfb1f0ff8311b8
leopard-gecko <23162102+leopard-gecko@users.noreply.github.com>
2026-06-20 14:51:49 +0900

    Update jrogue-clone.rb

--- jrogue-clone.rb
+++ jrogue-clone.rb
@@ -3,7 +3,7 @@ class JrogueClone < Formula
   homepage "https://leopard-gecko.github.io/jrogue/"
   url "https://github.com/leopard-gecko/homebrew-game/releases/download/vclone-iii.jec.001/rogue_clone_iii_jec_001.tar.gz"
   version "clone-iii.jec.001"
-  sha256 "7b14fad2a63b6dd058103f32e05869cfebe847e9adda425a3017854f973db6df"
+  sha256 "d86797f7c75b66ce66b85f669ca8517abed6e6df1fa7d2a23bca1f84d860920d"
 
   def install
     args = %W[
