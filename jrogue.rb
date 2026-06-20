7633421037e036e129c13a30e097c95484ebdd41
leopard-gecko <23162102+leopard-gecko@users.noreply.github.com>
2026-06-20 09:46:29 +0900

    Update jrogue.rb

--- jrogue.rb
+++ jrogue.rb
@@ -3,7 +3,7 @@ class Jrogue < Formula
   homepage "https://leopard-gecko.github.io/jrogue/"
   url "https://github.com/leopard-gecko/homebrew-game/releases/download/v5.4.5.jec.051/rogue_545_jec_051.tar.gz"
   version "5.4.5jec.051"
-  sha256 "40038107d03f3b0d32a05df3996344016d9c343fc5b561aa8085832a9904813e"
+  sha256 "7de114378a4f4bb5b1ff5fc61c13b849735de383eed17a01d9ce749f538c266e"
 
   def install
     args = %W[
